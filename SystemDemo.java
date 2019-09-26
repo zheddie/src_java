c23344cba1d9dddc71c60961d95c34dfcdfc590b
Gavin Zhang <zhanggan@cn.ibm.com>
2019-09-26 14:43:48 +0800

    ..

--- SystemDemo.java
+++ SystemDemo.java
@@ -6,9 +6,9 @@ public class SystemDemo {
    public static void main(String[] args) throws Exception {
     
       // create file
-      FileOutputStream f = new FileOutputStream("file.txt");
-
-      System.setOut(new PrintStream(f));
+//      FileOutputStream f = new FileOutputStream("file.txt");
+	PrintStream fileOut = new PrintStream("file2.txt");
+      System.setOut(fileOut);
 
       // this text will get redirected to file
       System.out.println("This is System class!!!");
